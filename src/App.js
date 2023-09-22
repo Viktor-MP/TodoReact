c7ce6958ee98ab53265b4a24aab2542506dce418
Viktor Melik-Parsadanyan <viktormelikparsadanyan@gmail.com>
2023-09-22 23:34:43 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import { Users } from "./Utiles/Utiles";
+import "./App.css";
 
-function App() {
+const App =() => {
+  console.log(Users)
+  const [users, setUsers] = useState(JSON.parse(localStorage.getItem("users")) || Users);
+
+  const [searchName, setSearchName] = useState("");
+
+  const [nextUserId, setNextUserId] = useState(users.length+1)
+
+
+  const userNameRef = useRef("");
+  const userSurNameRef = useRef("");
+
+
+  const addUserHendler = () => {
+    setNextUserId(nextUserId+1)
+    setUsers(
+      users.concat({
+        name: userNameRef.current.value,
+        surname: userSurNameRef.current.value,
+        id: nextUserId,
+      })
+    );
+  };
+
+  const deleteUserHendler = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  
+  const changeNameHandler = (event) => {
+    setSearchName(event.target.value);
+  };
+
+  const deleteAllUsers = () => {
+    const filtered = users.filter((el) => !el.isChecked);
+    setUsers(filtered);
+  };
+
+  const filterByName = (item) => {
+    return item.name
+      .toLocaleLowerCase()
+      .includes(searchName.toLocaleLowerCase());
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">
+        <div className="addInputs">
+        <input ref={userNameRef} placeholder="name" />
+        <input ref={userSurNameRef} placeholder="surname" />
+        <button onClick={addUserHendler}>Add User</button>
+      </div>
+      <div className="searchUser">
+        <input onChange={changeNameHandler} placeholder="serch users" />
+      </div>
+      </div>
+      <div className="body">
+        <ul type="none" className="list">
+          {localStorage.setItem('users', JSON.stringify(users))}
+          {users.filter(filterByName).map((user) => {
+            return (
+              <li id={user.id} className="listItem" key={user.id}>
+                <input
+                  onChange={() => (user.isChecked = !user.isChecked)}
+                  type="checkbox"
+                />
+                <p>{user.name}</p>
+                <p className="textOverflow">{user.surname}</p>
+                <button   onClick={() => deleteUserHendler(user.id)}>X</button>
+              </li>
+            );
+          })}
+      <button className="deleteAllUsers" onClick={deleteAllUsers}> Delete all cheked users </button>
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/Utiles/Utiles.js
@@ -0,0 +1,12 @@
+
+
+const Users = [
+  
+  { name: "Narek", surname: "Bloyan", id: 1, isChecked: false },
+  { name: "Blo", surname: "Henoyan", id: 2, isChecked: false },
+  { name: "Heno", surname: "Suroyan", id: 3, isChecked: false },
+  { name: "Suro", surname: "Varoyan", id: 4, isChecked: false },
+  { name: "Varo", surname: "Narekyan", id: 5, isChecked: false },
+  ]
+
+  export {Users}
